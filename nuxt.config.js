b222d9a4a008df4cdab7c935d672a88768dac3d5
Kamontat Chantrachirathumrong <kamontat.c@hotmail.com>
2018-12-20 14:23:49 +0100

    feat(head): add author version and somemeta data

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,10 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'name', name: 'name', content: pkg.name },
+      { hid: 'description', name: 'description', content: pkg.description },
+      { hid: 'author', name: 'author', content: pkg.author },
+      { hid: 'version', name: 'version', content: pkg.version }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
